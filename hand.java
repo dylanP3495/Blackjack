4fa94e84dfb504062a3360b696174370ecd4072b
Dylan <palafoxdylan@gmail.com>
2017-10-02 14:05:58 -0700

    10/2

---
+++ hand.java
@@ -0,0 +1,30 @@
+
+/**
+ * It does a thing.
+ * 
+ * @author Dylan
+ * @version 9/27/17
+ */
+
+import java.util.Scanner;
+import java.lang.Math;
+public class hand
+{
+    int randomNum =((int)(Math.random() * 9)+2);
+    public int startHand = randomNum;
+    public void playerHand(){
+
+    }
+
+    public void dealerHand(){
+
+    }
+
+    public void setHit(){
+
+    }
+
+    public void setStay(){
+
+    }
+}
